3cab838e5d8eddf2b75704559c0a1a82d2ad66d8
Terry <b0972834939@gmail.com>
2022-10-13 15:19:09 +0800

    refactor: 將Audio的Ref拉升到App.js方便之後取值使用

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "primeicons/primeicons.css";
 import "primereact/resources/themes/lara-dark-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
@@ -16,12 +16,87 @@ const audios = [
 
 const App = () => {
 	const [mobileSidebarActive, setMobileSidebarActive] = useState(false);
-	const [selectedVideo, setSelectedVideo] = useState(audios[0]);
+	// const [selectedVideo, setSelectedVideo] = useState(audios[0]);
+	const [audioTime, setAudioTime] = useState(0);
+	const [totalSecond, setTotalSecond] = useState(0);
+	const [play, setPlay] = useState(false);
+	const [volume, setVolume] = useState(0.15);
+
 	// TODO：查詢API資料 const [audios, setAudios] = useState(null);
+	const audio = useRef();
+	const audioContextRef = useRef();
+	const gainNodeRef = useRef();
 
 	useEffect(() => {
-		setSelectedVideo(audios[0]);
+		// 新增一個audio的HTML Object
+		audio.current = new Audio(audios[0]);
+		const audioContext = new AudioContext();
+
+		// 設定audio事件
+		audio.current.addEventListener("loadedmetadata", () => {
+			audio.current.ontimeupdate = updateTime;
+			audio.current.onended = () => {
+				setPlay(false);
+			};
+			setTotalSecond(audio.current.duration);
+		});
+
+		// 加入Node
+		const track = audioContext.createMediaElementSource(audio.current);
+
+		// 調整音量
+		gainNodeRef.current = audioContext.createGain();
+
+		// connect要做的事
+		track.connect(gainNodeRef.current).connect(audioContext.destination);
+
+		audioContextRef.current = audioContext;
+
+		return () => {
+			audioContextRef.current.close();
+			audio.current = null;
+			setPlay(false);
+		};
 	}, []);
+
+	useEffect(() => {
+		gainNodeRef.current.gain.value = volume;
+	}, [volume]);
+
+	useEffect(() => {
+		if (audioContextRef.current.state === "suspended") {
+			audioContextRef.current.resume();
+		}
+
+		if (play) {
+			audio.current.play();
+		} else {
+			audio.current.pause();
+		}
+	}, [play]);
+
+	const updateTime = (e) => {
+		setAudioTime(e.target.currentTime);
+	};
+
+	const silderTimeChange = (e) => {
+		if (audio.current.ontimeupdate) {
+			audio.current.ontimeupdate = null;
+		}
+
+		setAudioTime(e.value);
+	};
+
+	const sliderTimeChangeEnd = (e) => {
+		audio.current.currentTime = e.value;
+		audio.current.ontimeupdate = updateTime;
+
+		if (play) {
+			audio.current.play();
+		} else {
+			audio.current.pause();
+		}
+	};
 
 	return (
 		<div
@@ -40,7 +115,16 @@ const App = () => {
 					<AppContent />
 				</div>
 				<div className="flex flex-wrap align-items-center justify-content-center h-8rem layout-footer audio-player">
-					<AudioPlayer selectedVideo={selectedVideo} />
+					<AudioPlayer
+						totalSecond={totalSecond}
+						audioTime={audioTime}
+						play={play}
+						setPlay={setPlay}
+						volume={volume}
+						setVolume={setVolume}
+						silderTimeChange={silderTimeChange}
+						sliderTimeChangeEnd={sliderTimeChangeEnd}
+					/>
 				</div>
 			</div>
 		</div>
--- src/components/AudioPlayers/AudioPlayer.js
+++ src/components/AudioPlayers/AudioPlayer.js
@@ -1,105 +1,27 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "primereact/button";
 import { Slider } from "primereact/slider";
 
-const AudioPlayer = ({ selectedVideo }) => {
-	const [audioTime, setAudioTime] = useState(0);
-	const [totalSecond, setTotalSecond] = useState(0);
+const AudioPlayer = ({
+	totalSecond,
+	audioTime,
+	play,
+	setPlay,
+	volume,
+	setVolume,
+	silderTimeChange,
+	sliderTimeChangeEnd,
+}) => {
+	const [audioTotalTime, setaudioTotalTime] = useState("0:00");
 	const [currentTime, setCurrentTime] = useState("0:00");
-	const [audioTotalTime, setaudioTotalTime] = useState("");
-	const [volume, setVolume] = useState(1);
-	const [play, setPlay] = useState(false);
-	const audio = useRef();
-	const audioContextRef = useRef();
-	const gainNodeRef = useRef();
 
 	useEffect(() => {
-		// 新增一個audio的HTML Object
-		audio.current = new Audio(selectedVideo);
-		const audioContext = new AudioContext();
-
-		audio.current.addEventListener("loadedmetadata", () => {
-			audio.current.ontimeupdate = updateTime;
-			audio.current.onended = () => {
-				setPlay(false);
-			};
-			setaudioTotalTime(calculateTimeToString(audio.current.duration));
-			setTotalSecond(audio.current.duration);
-		});
-
-		// 加入Node
-		const track = audioContext.createMediaElementSource(audio.current);
-
-		// 調整音量
-		gainNodeRef.current = audioContext.createGain();
-
-		// TODO:加入分析
-		// const analyser = audioContext.createAnalyser();
-		// var distortion = audioContext.createWaveShaper();
-		// analyser.connect(distortion);
-		// distortion.connect(audioContext.destination);
-		// analyser.fftSize = 2048;
-		// const bufferLength = analyser.frequencyBinCount;
-		// const dataArray = new Uint8Array(bufferLength);
-		// analyser.getByteTimeDomainData(dataArray);
-
-		// connect要做的事
-		track
-			.connect(gainNodeRef.current)
-			// TODO: 上面加入分析寫完後要做的 .connect(analyser)
-			.connect(audioContext.destination);
-
-		audioContextRef.current = audioContext;
-
-		return () => {
-			audioContextRef.current.close();
-			audio.current = null;
-			setPlay(false);
-		};
-	}, [selectedVideo]);
+		setaudioTotalTime(calculateTimeToString(totalSecond));
+	}, [totalSecond]);
 
 	useEffect(() => {
 		setCurrentTime(calculateTimeToString(audioTime));
 	}, [audioTime]);
-
-	useEffect(() => {
-		gainNodeRef.current.gain.value = volume;
-	}, [volume]);
-
-	useEffect(() => {
-		if (audioContextRef.current.state === "suspended") {
-			audioContextRef.current.resume();
-		}
-
-		if (play) {
-			audio.current.play();
-		} else {
-			audio.current.pause();
-		}
-	}, [play]);
-
-	const updateTime = (e) => {
-		setAudioTime(e.target.currentTime);
-	};
-
-	const silderTimeChange = (e) => {
-		if (audio.current.ontimeupdate) {
-			audio.current.ontimeupdate = null;
-		}
-
-		setAudioTime(e.value);
-	};
-
-	const sliderTimeChangeEnd = (e) => {
-		audio.current.currentTime = e.value;
-		audio.current.ontimeupdate = updateTime;
-
-		if (play) {
-			audio.current.play();
-		} else {
-			audio.current.pause();
-		}
-	};
 
 	const calculateTimeToString = (time) => {
 		const minutes = Math.floor(time / 60);
@@ -113,7 +35,7 @@ const AudioPlayer = ({ selectedVideo }) => {
 			<Button
 				icon={`pi ${play ? "pi-pause" : "pi-play"}`}
 				className="p-button-rounded p-button-secondary"
-				aria-label="Bookmark"
+				aria-label="Player"
 				onClick={() => setPlay(!play)}
 			/>
 			<div className="flex align-items-center justify-content-center audio-central">
